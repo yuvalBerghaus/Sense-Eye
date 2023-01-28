fda5fc9eb6a2ed7445a26517374f612c0473c47c
yuvalBerghaus <yuvalBerghaus>
2023-01-28 19:01:53 +0200

    finished

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 from flask import Flask, request
-
+import requests
 app = Flask(__name__)
+
+mapping_color_to_ip = {}
+mapping_ip_to_color = {}
+current_player_with_the_ball_color_ip = None  # we will have his ip in here
+
+
+@app.route('/send_recommendation_to_color') # output D1-GPIO5-5 D2-GPIO4-4 D3-GPIO0-0 D4-GPIO2-2
+def recommend():
+    color = request.args.get('color')
+    output_state = request.args.get('output_state')  # output D1-GPIO5-5 D2-GPIO4-4 D3-GPIO0-0 D4-GPIO2-2
+    current_player_with_the_ball_ip = mapping_color_to_ip[color]
+    print('http://'+current_player_with_the_ball_ip+'/recommend/'+output_state)
+    requests.get('http://'+current_player_with_the_ball_ip+'/recommend/'+output_state)
+    # Do something with the received data
+    return "Data received"
 
 
 @app.route('/save_id')
 def save_id():
     color_band = request.args.get('color_band')
     ip = request.args.get('ip')
+    mapping_color_to_ip[color_band] = ip
+    mapping_ip_to_color[ip] = color_band
     print("ip is = " + ip)
     print("color_band is = " + color_band)
     # Do something with the received data
+    return "Data received"
+
+
+# @app.route('/help_button')
+# def help_button():
+#     color_band = request.args.get('color_band')
+#     ip = request.args.get('ip')
+#
+#     print("ip is = " + ip)
+#     print("color_band is = " + color_band)
+#     # Do something with the received data
+#     return "Data received"
+
+
+@app.route('/status')
+def show_connected_players():
+    for key in mapping_color_to_ip:
+        print(key)
     return "Data received"
 
 
--- sketch_nov29a_copy_20230127154538/sketch_nov29a_copy_20230127154538.ino
+++ sketch_nov29a_copy_20230127154538/sketch_nov29a_copy_20230127154538.ino
@@ -1,18 +1,18 @@
-// Load Wi-Fi library
 #include <ESP8266WiFi.h>
-
+#include <ESP8266WebServer.h>
 // Replace with your network credentials
 const char* ssid     = "Yuval";
 const char* password = "Panama507";
+bool pressed = false;
+int recommended_index;
 
 // Set web server port number to 80
-WiFiServer server(80);
+ESP8266WebServer server(80);
 // Host
 const char* host = "192.168.14.3";
 WiFiClient client_to_server;
 // Variable to store the HTTP request
 String header;
-
 // Auxiliar variables to store the current output state
 String output5State = "off";
 String output4State = "off";
@@ -20,16 +20,19 @@ String output0State = "off";
 String output2State = "off";
 // Assign output variables to GPIO pins
 const int output5 = 5; // HP
-const int output4 = 4;
-const int output0 = 0;
-const int output2 = 2;
+const int output4 = 4;    //GPIO4
+const int output0 = 0;    //GPIO0
+const int output2 = 2;    //GPIO2
+const char input13 = 13; // the button GPIO16
 // Current time
 unsigned long currentTime = millis();
 // Previous time
 unsigned long previousTime = 0; 
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
-String url = null;
+String url;
+
+
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
@@ -37,6 +40,7 @@ void setup() {
   pinMode(output4, OUTPUT);
   pinMode(output0, OUTPUT);
   pinMode(output2, OUTPUT);
+  pinMode(input13, INPUT_PULLUP);
   // Set outputs to LOW
   digitalWrite(output5, LOW);
   digitalWrite(output4, LOW);
@@ -83,157 +87,73 @@ void setup() {
   client_to_server.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
-
+client_to_server.stop();
 ///////////////////////////////////////////////////////////////////////////////
 
+  delay(100);
+  
+  server.on("/recommend/5", handle_5);
+  server.on("/recommend/4", handle_4);
+  server.on("/recommend/0", handle_0);
+  server.on("/recommend/2", handle_2);
   server.begin();
 }
 
 void loop(){
-
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
-            }
-            else if (header.indexOf("GET /0/on") >= 0) {
-              Serial.println("GPIO 0 on");
-              output0State = "on";
-              digitalWrite(output0, HIGH);
-            } else if (header.indexOf("GET /0/off") >= 0) {
-              Serial.println("GPIO 0 off");
-              output0State = "off";
-              digitalWrite(output0, LOW);
-            }
-            
-            else if (header.indexOf("GET /2/on") >= 0) {
-              Serial.println("GPIO 2 on");
-              output2State = "on";
-              digitalWrite(output2, HIGH);
-            } else if (header.indexOf("GET /2/off") >= 0) {
-              Serial.println("GPIO 2 off");
-              output2State = "off";
-              digitalWrite(output2, LOW);
-            }
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-
-
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 0 - State " + output0State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output0State=="off") {
-              client.println("<p><a href=\"/0/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/0/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 2 - State " + output2State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output2State=="off") {
-              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
+  server.handleClient();
+  bool currentState = digitalRead(input13);
+  if(currentState == pressed) {
+    Serial.println("Hello");
+    turn_recommended_buzzer();
+    while(digitalRead(input13) == pressed) {
+      // Do nothing
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
   }
-  if(client_to_server) {
-      unsigned long timeout = millis();
-      while (client_to_server.available() == 0) {
-        if (millis() - timeout > 5000) {
-          Serial.println(">>> Client Timeout !");
-          client_to_server.stop();
-          return;
-        }
-      }
-  }
+}
+
+
+void handle_5() {
+    // output5State = "on";
+    recommended_index = 5;
+    server.send(200, "text/html", "hello from esp8266!"); 
+    // digitalWrite(output5, HIGH);
+    // delay(1000);
+    // output5State = "off";
+    // digitalWrite(output5, LOW);
+}
+
+void handle_4() {
+    recommended_index = 4;
+    server.send(200, "text/html", "hello from esp8266!"); 
+    // output4State = "on";
+    // digitalWrite(output4, HIGH);
+    // delay(1000);
+    // output5State = "off";
+    // digitalWrite(output4, LOW);
+}
+void handle_0() {
+    recommended_index = 0;
+    server.send(200, "text/html", "hello from esp8266!"); 
+    // output0State = "on";
+    // digitalWrite(output0, HIGH);
+    // delay(1000);
+    // output5State = "off";
+    // digitalWrite(output0, LOW);
+}
+
+void handle_2() {
+    recommended_index = 2;
+    server.send(200, "text/html", "hello from esp8266!"); 
+    // output2State = "on";
+    // digitalWrite(output2, HIGH);
+    // delay(1000);
+    // output2State = "off";
+    // digitalWrite(output2, LOW);
+}
+
+void turn_recommended_buzzer() {
+  digitalWrite(recommended_index, HIGH);
+  delay(1000);
+  digitalWrite(recommended_index, LOW);
+  
 }
